7e9f5cdc653443082ae3f84fcdba62cb3f4ebfca
Abhishek Singh <abhi0singh1@gmail.com>
2026-08-10 10:14:09 +0530

    matrix addition

--- add_matrix.cu
+++ add_matrix.cu
@@ -2,18 +2,23 @@
 #include <iostream>
 #include <vector>
 
-__global__ void addMatrix(const int* a, const int*b, int* c, int rows, int cols){
-    int col = blockIdx.x * blockDim.x + threadIdx.x;
-    int row = blockIdx.y * blockDim.y + threadIdx.y;
+__global__ void addMatrices(const int* a,const int* b,int* c,int rows,int columns)
+{ 
+    int column =blockIdx.x * blockDim.x + threadIdx.x;
 
-    if (row < rows && col < cols){
-        c[row * cols + col] = a[row * cols + col] + b[row * cols + col];
+    int row =blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (row < rows && column < columns) {
+        int index = row * columns + column;
+
+        c[index] = a[index] + b[index];
     }
 }
 
-int main(){
+int main()
+{
     const int rows = 3;
-    const int cols = 4;
+    const int columns = 4;
     const int totalElements = rows * columns;
     const size_t bytes = totalElements * sizeof(int);
 
@@ -29,13 +34,70 @@ int main(){
         90, 100, 110, 120
     };
 
-    std::vector<int> C(rows * cols);
-    int *d_A, *d_B, *d_C = nullptr;
-    cudaMalloc(&d_A, bytes);
-    cudaMalloc(&d_B, bytes);
-    cudaMalloc(&d_C, bytes);
+    std::vector<int> C(totalElements, 0);
 
-    cudaMemcpy(d_A,A.data(),bytes, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_B,B.data(),bytes, cudaMemcpyHostToDevice);
-    return 0 ;
+    int* deviceA = nullptr;
+    int* deviceB = nullptr;
+    int* deviceC = nullptr;
+
+    cudaMalloc(&deviceA, bytes);
+    cudaMalloc(&deviceB, bytes);
+    cudaMalloc(&deviceC, bytes);
+
+    cudaMemcpy(deviceA,A.data(),bytes,cudaMemcpyHostToDevice);
+
+    cudaMemcpy(deviceB,B.data(),bytes,cudaMemcpyHostToDevice);
+
+    // 2 threads in x direction and 2 threads in y direction
+    dim3 threadsPerBlock(2, 2);
+
+    dim3 numberOfBlocks(
+        (columns + threadsPerBlock.x - 1)
+            / threadsPerBlock.x,
+
+        (rows + threadsPerBlock.y - 1)
+            / threadsPerBlock.y
+    );
+
+    addMatrices<<<numberOfBlocks, threadsPerBlock>>>(
+        deviceA,
+        deviceB,
+        deviceC,
+        rows,
+        columns);
+
+    cudaError_t error = cudaDeviceSynchronize();
+
+    if (error != cudaSuccess) {
+        std::cerr
+            << "CUDA error: "
+            << cudaGetErrorString(error)
+            << "\n";
+
+        return 1;
+    }
+
+    cudaMemcpy(
+        C.data(),
+        deviceC,
+        bytes,
+        cudaMemcpyDeviceToHost);
+
+    std::cout << "Result matrix:\n";
+
+    for (int row = 0; row < rows; ++row) {
+        for (int column = 0; column < columns; ++column) {
+            int index = row * columns + column;
+
+            std::cout << C[index] << "\t";
+        }
+
+        std::cout << "\n";
+    }
+
+    cudaFree(deviceA);
+    cudaFree(deviceB);
+    cudaFree(deviceC);
+
+    return 0;
 }
